6e7cd80028202f7149fbba445e818b8e22d41e89
Michail Martinis <mixamart98@gmail.com>
2023-04-21 20:29:36 +0300

    because of jenkins fails,maybe need docker info

--- variables.tf
+++ variables.tf
@@ -1,3 +1,12 @@
 variable "location" {}
 variable "admin_username" {}
 variable "admin_password" {}
+variable "docker_image" {
+  type    = string
+  default = "martmich/todoappwithlogin"
+}
+
+variable "docker_tag" {
+  type    = string
+  default = "latest"
+}
