c377b2f5c0706289be194505478b8a7c9e314e60
Michail Martinis <mixamart98@gmail.com>
2023-04-21 21:06:20 +0300

    add inside variable.tf the type

--- variables.tf
+++ variables.tf
@@ -1,5 +1,14 @@
 variable "location" {}
 variable "admin_username" {}
 variable "admin_password" {}
-variable "docker_image" {}
-variable "docker_tag" {}
+variable "docker_image" {
+  type = string
+}
+
+variable "docker_tag" {
+  type = string
+}
+variable "location" {
+  type = string
+  default = "northeurope"
+}
