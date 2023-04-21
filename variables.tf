34f787293c8b9740a08b4b60d02759913063eb9a
Michail Martinis <mixamart98@gmail.com>
2023-04-21 21:19:26 +0300

    Make it as the begining and have the vars in the code

--- variables.tf
+++ variables.tf
@@ -1,14 +1,3 @@
 variable "location" {}
 variable "admin_username" {}
 variable "admin_password" {}
-variable "docker_image" {
-  type = string
-}
-
-variable "docker_tag" {
-  type = string
-}
-variable "location" {
-  type = string
-  default = "northeurope"
-}
