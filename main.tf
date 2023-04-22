c401cdaf3164e74fdddd535851406d5db52b0aed
Michail Martinis <mixamart98@gmail.com>
2023-04-22 22:00:42 +0300

    a change in the code to see if it will trigger tf

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "rg"{                    #resource group is called "main"???
 
-  name = "project-codehub-reg-test"
+  name = "project-codehub-reg-test-2"
   location = var.location
 }
 
