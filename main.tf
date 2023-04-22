82a83acc55a0e77a9c8838f686007ca67e01bffa
martmicha <mmartinis@athtech.gr>
2023-04-23 00:11:09 +0300

    the previous didnt work

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "rg"{                    #resource group is called "main"???
 
-  name = "project-codehub-reg"
+  name = "project-codehub-reg2"
   location = var.location
 }
 
